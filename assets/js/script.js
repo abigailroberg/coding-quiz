3c919879c49923d77d2fa09f1fd6a4a0fd647738
Abby Roberg <abbyroberg@gmail.com>
2021-07-13 18:19:16 -0500

    initial styling and page set up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,19 @@
+// 
+var highScoreBtnEl = document.querySelector("#high-score-btn");
+var timeEl = document.querySelector("#time-left");
+var newQuestionBtnEl = document.querySelector("#new-question-btn");
+var quizEl = document.querySelector("#quiz");
+
 // global variables
 let timeLeft = 60;
 let score = 0;
 
 // start function
 
-// ask question function
+// ask new squestion function
 
-// timer function
+// view high scores function
 
 // event listener for click start
 // event listener for answer selection
+// event listener for high scores

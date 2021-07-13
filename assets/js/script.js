7905f5741d25fb89048bbfc90be19f98388403a4
Abby Roberg <abbyroberg@gmail.com>
2021-07-13 17:40:06 -0500

    initial structure

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// global variables
+let timeLeft = 60;
+let score = 0;
+
+// start function
+
+// ask question function
+
+// timer function
+
+// event listener for click start
+// event listener for answer selection
